c90678734574a3f79f5c53d8b36a801a1a446bee
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-10-02 11:21:31 +0530

    Hacktoberfest 2022 in C# printed

---
+++ C#/demo.cs
@@ -0,0 +1,9 @@
+namespace Hacktoberfest
+{
+    class demo {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hacktoberfest 2022");
+        }
+    }
+}
